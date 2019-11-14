bdb6a5bea02ff4fe7773923f9a33530ff0e87442
unknown <erwin@gostyle.studio>
2019-11-15 00:10:26 +0800

    new build

---
+++ build/precache-manifest.913a02cae39da517205e16bd8d138566.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f4b551efa1a9c6f9222b28ca2fb7d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "873d2e6b7426d41c30f3",
+    "url": "/static/css/2.5adff6bb.chunk.css"
+  },
+  {
+    "revision": "8d4e341089c2a4ecf61b",
+    "url": "/static/css/main.4649a9b7.chunk.css"
+  },
+  {
+    "revision": "873d2e6b7426d41c30f3",
+    "url": "/static/js/2.7aa4ed81.chunk.js"
+  },
+  {
+    "revision": "8d4e341089c2a4ecf61b",
+    "url": "/static/js/main.94e422cf.chunk.js"
+  },
+  {
+    "revision": "1427ec3227392ee08d3c",
+    "url": "/static/js/runtime-main.c7249c18.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
