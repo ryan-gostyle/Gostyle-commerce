a975ab14787e6e2d0de6fee185a6261ba08e1550
RynRam <ryankenethramiso@gmail.com>
2019-11-25 18:22:37 +0800

    footer and header logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,76 @@
 import React, { Component } from 'react'
+import './Footer.css'
+import Newsletter from './Newsletter'
+import { Icon } from 'antd';
 
 export default class Footer extends Component {
     render() {
         return (
-            <div style={{ backgroundColor: '#000',height:50}}>
-                
-            </div>
+<div className="section bg-dark footer"> 
+		<div className="container">
+			<div className="row">
+	
+				<div className="col-12 col-md-3">
+					<ul>
+						<li className="list-header">About</li>
+						<li><a href="/how-we-work/">How we work</a></li>
+						<li><a href="/services/">Services</a></li>
+						<li><a href="/inspiration/">Get inspired</a></li>
+						<li><a href="/book-a-specialist/">Book a specialist</a></li>
+						<li><a href="/articles">Blog</a></li>
+						<li><a href="/events">Events</a></li>
+						<li><a href="/work-with-us">Careers</a></li>
+					</ul>
+				</div>
+				<div className="col-12 col-md-3">
+					<ul>
+						<li className="list-header">Marketing Services</li>
+						<li><a href="/service/branding/">Branding</a></li>
+						<li><a href="/service/web-development/">Web development</a></li>
+						<li><a href="/service/video/">Video</a></li>
+						<li><a href="/service/photography/">Photography</a></li>
+						<li><a href="/service/graphic-design/">Graphic design</a></li>
+						<li><a href="/service/social-media/">Social media</a></li>
+						<li><a href="/service/digital-copy/">Digital copy</a></li>
+						<li><a href="/service/marketing-strategy/">Marketing strategy</a></li>
+					</ul>
+				</div>
+				<div className="col-12 col-md-3">
+					<ul>
+						<li className="list-header"><a href="https://www.buzzbar.co/contact">Contact</a></li>
+						<li><a href="mailto:Support@gostyle.studio">Support@gostyle.studio</a></li>
+						<li><a href="tel:02039280900">09* **** ****</a></li>
+						<li><a href="/#booking">My account</a></li>
+						<li><a href="/privacy-policy">Privacy policy</a></li>
+						<li><a href="/terms-and-conditions">Terms &amp; conditions</a></li>
+					</ul>
+				</div>
+				<div className="col-12 col-md-3 text-left text-md-right">
+					<div id="mc_embed_signup">
+                        <ul>
+                            <li className="list-header">Keep up to date with the <br/>latest news</li>
+                        </ul>
+						<Newsletter/>
+					</div>
+				</div>
+			</div>
+			<div className="row mt-5 align-items-center">
+				<div className="col-12 col-md">
+					<a href="/" className="social-icon" aria-label="Instagram"><Icon type="instagram"/></a>	
+					<a href="/" className="social-icon" aria-label="Facebook"><Icon type="facebook" theme="filled" /></a>
+					<a href="/" className="social-icon" aria-label="Twitter"><Icon type="twitter" /></a>
+					<a href="/" className="social-icon" aria-label="Linkedin"><Icon type="linkedin" theme="filled" /></a>  
+				</div>
+				<div className="col-12 col-md text-left text-md-center">
+					{/* <a href="https://w3w.co/vocal.teach.wishes" className="social-icon" style={{fontSize:"16px !important;"}}>///vocal.teach.wishes</a> */}
+				</div>
+				<div className="col-12 col-md text-left text-md-right">
+					<span className="capitalize" style={{color: "#fff"}}>© Gostyle Studio 2019</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
         )
     }
 }
---
+++ src/components/Newsletter.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react'
+import {
+  Form,
+  Input,
+  Button,
+} from 'antd';
+// import Api from '../../../Api';
+
+import Axios from 'axios';
+
+
+
+class Newsletters extends Component {
+
+  state = {
+    time: null,
+    date: null,
+    timezones: [],
+  }
+  onChange = (time, timeString) => {
+    this.setState({ time: timeString });
+  }
+  onChange1 = (time, timeString) => {
+    // console.log(timeString);
+    this.setState({ date: timeString });
+  }
+
+  async componentDidMount() {
+
+    var timezone = await Axios.get('https://raw.githubusercontent.com/dmfilipenko/timezones.json/master/timezones.json');
+    this.setState({ timezones: timezone.data })
+  }
+
+
+
+//   handleSubmit = async e => {
+//     e.preventDefault();
+
+//     this.props.form.validateFieldsAndScroll(async (err, values) => {
+//       if (!err) {
+//         var submit = await Api.post('register', {
+//           name: values.fullname,
+//           brand_company: values.brand,
+//           phone_number: "09" + values.phone,
+//           email: values.email,
+//           package: values.package,
+//           password: values.email,
+//           password_confirmation: values.email,
+//           status: "active",
+//           location: "Makati",
+//           place: values.place,
+//           date: this.state.date,
+//           time: this.state.time,
+//           timezone: values.timezone
+//         })
+//           .catch(function (error) {
+//             if (error.response) {
+//               // The request was made and the server responded with a status code
+//               // that falls out of the range of 2xx
+//               console.log(error.response);
+//               console.log(error.response.data);
+//               console.log(error.response.status);
+//               console.log(error.response.headers);
+//               Object.keys(error.response.data).map(function (key, index) {
+//                 MySwal.fire({
+//                   title: "Error",
+//                   text: error.response.data[key][0],
+//                   icon: 'error',
+//                 });
+//                 return false
+//               });
+//               // currentComponent.setState({ status: "error", message: error.response.data })
+//             } else {
+//               MySwal.fire({
+//                 title: "Error",
+//                 text: "Error Try Again later",
+//                 icon: 'error',
+//               });
+//             }
+//           });
+//         if (await submit) {
+//           MySwal.fire({
+//             title: "Booked!",
+//             text: "We'll get back to you, Check your mail ;)",
+//             icon: "success"
+//           });
+//         }
+//       }
+//     });
+//   };
+
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Item>
+          {getFieldDecorator('email', {
+            rules: [
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ],
+          })(<Input placeholder='Email' />)}
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{borderRadius:25,margin: "15px 5px"}}>
+            Subscribe
+              </Button>
+        </Form.Item>
+      </Form>
+    )
+  }
+}
+
+const Newsletter = Form.create({ name: 'newletter' })(Newsletters);
+export default Newsletter
