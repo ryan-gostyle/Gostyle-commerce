cb0dbe94054289e5a1a7f311a73cdafa741fdc92
unknown <erwin@gostyle.studio>
2019-12-05 10:29:57 +0800

    fix commerce

--- src/components/client/ecommerce/Collaborate.js
+++ src/components/client/ecommerce/Collaborate.js
@@ -12,17 +12,21 @@ export default class Collaborate extends Component {
                         <img alt="no ta.vin" src="https://lynagails-caters.s3-ap-southeast-1.amazonaws.com/uploads/Gostyle/ta.vin.png" />
                     </Col>
                     <Col xs={12} md={6} lg={6}>
-                        <h2 style={{ fontFamily: "proxima-bold", textAlign: "center", fontSize:"2.5em"}}>CASE STUDY:<span className="dedicated" style={{ fontFamily: "proxima"}}> Ta.Vin Moodboard</span> </h2>
-                        <p>Want to keep tabs on your project? Check in on the progress,access your invoices, view documents, book new appointments and more on the Gostyle App</p>
+                        <h2 style={{ fontFamily: "proxima-bold", textAlign: "center", fontSize: "2.5em" }}>CASE STUDY:<span className="dedicated" style={{ fontFamily: "proxima" }}> Ta.Vin Moodboard</span> </h2>
+                        <p>Ta.vin is a marketplace for local brands in the Philippines. They strive to empower Filipino entreprenuers and artisans with their platform. We have collaborated with the founder from ideation to execution, giving life to her vision - Tavin Moodboard with our shopify custom solution.</p>
                         <br />
                         <div>
-                            <Anchor affix={false} style={{ display: "inline-block" }}>
-                                <Link href="#solution" className="we-work" title="Learn More" />
-                            </Anchor>
-                            <Anchor affix={false} style={{ display: "inline-block" }}>
-                                <Link href="#booking" className="book-now" title="Book Now" />
-                            </Anchor>    
-                        </div> 
+                            <Row middle="xs" center="xs">
+                                <Col xs={12} md={3} lg={3}>
+                                    <a href="//www.ta.vin" style={{paddingTop:'15px', paddingBottom:"15px"}}  className="book-now">www.Ta.vin</a>
+                                </Col>
+                                <Col xs={12} md={3} lg={3}>
+                                    <Anchor affix={false} >
+                                        <Link href="#solution1" className="book-now" title="Read More" />
+                                    </Anchor>
+                                </Col>
+                            </Row>
+                        </div>
                     </Col>
 
                 </Row>
--- src/components/client/ecommerce/Wework.js
+++ src/components/client/ecommerce/Wework.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import ShowMore from 'react-show-more';
 import '../landingv3/Nutshell.css';
 import './FeaturedWorks.css';
 import { Anchor } from 'antd';
@@ -23,12 +24,32 @@ export default class Wework extends Component {
                                         <div className="item  fade-in-bottom-1 ">
                                             <img alt="no ecom1.png" src="https://lynagails-caters.s3-ap-southeast-1.amazonaws.com/uploads/Gostyle/ecommerce-01.png" />
                                             <p>ECOMMERCE Website Development</p>
-                                            <ul>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                            </ul>
+                                            <ShowMore
+                                                lines={3}
+                                                more='Show more'
+                                                less='Show less'
+                                                anchorClass='showmore'
+                                            >
+                                                <ul>
+                                                    <li>3 Months FREE of Technical Support</li>
+                                                    <li> Upload of up to unlimited products & unlimited categories</li>
+                                                    <li> Setup of newsletter form with pop-up and/or top bar</li>
+                                                    <li> shipping settings</li>
+                                                    <li> Redirect domain and point nameservers</li>
+                                                    <li> Social media links and share links</li>
+                                                    <li> Instagram feed or shop Instagram widget</li>
+                                                    <li> Install Google Analytics tracking snippet</li>
+                                                    <li> Submission of sitemap to Google Search Console</li>
+                                                    <li> SEO: Search engine optimisation for up to 50 products, collection pages, and homepage</li>
+                                                    <li> Setup of shopping cart</li>
+                                                    <li> Setup of up to twelve (12) info pages & one contact form</li>
+                                                    <li> Custom responsive, desktop, mobile & tablet friendly web design</li>
+                                                    <li> Two (2) rounds of major revisions</li>
+                                                    <li> Setup of navigation, homepage and footer</li>
+                                                    <li> Access to eCommerce Partners</li>
+                                                    <li> Custom Website Application</li>
+                                                </ul>
+                                            </ShowMore>
                                             <Anchor affix={false} style={{ display: "inline-block" }}>
                                                 <Link href="#booking" className="book-now" title="Request Quote" />
                                             </Anchor>
@@ -38,12 +59,23 @@ export default class Wework extends Component {
                                         <div className="item  fade-in-bottom-2">
                                             <img alt="no inbounmarketing" src="https://lynagails-caters.s3-ap-southeast-1.amazonaws.com/uploads/Gostyle/inbound+marketing.png" />
                                             <p>ECOMMERCE Inbound Marketing</p>
-                                            <ul>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                            </ul>
+                                            <ShowMore
+                                                lines={3}
+                                                more='Show more'
+                                                less='Show less'
+                                                anchorClass='showmore'
+                                            >
+                                                <ul>
+                                                    <li>Dedicated Graphic Designer</li>
+                                                    <li>Social Media Posts</li>
+                                                    <li>Facebook Ads Optimization</li>
+                                                    <li>Copywriting</li>
+                                                    <li>Social Media Optimization</li>
+                                                    <li>SEO: for up to 50 oriducts,collection,page, and homepage</li>
+                                                    <li>Google Ads</li>
+                                                    <li>Product Optimization</li>
+                                                </ul>
+                                            </ShowMore>
                                             <Anchor affix={false} style={{ display: "inline-block" }}>
                                                 <Link href="#booking" className="book-now" title="Request Quote" />
                                             </Anchor>
@@ -54,12 +86,23 @@ export default class Wework extends Component {
                                         <div className="item   fade-in-bottom-3">
                                             <img alt="no mobile" src="https://lynagails-caters.s3-ap-southeast-1.amazonaws.com/uploads/Gostyle/mobile.png" />
                                             <p>ECOMMERCE Virtual Assistant</p>
-                                            <ul>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                            </ul>
+                                            <ShowMore
+                                                lines={3}
+                                                more='Show more'
+                                                less='Show less'
+                                                anchorClass='showmore'
+                                            ><ul>
+                                                    <li>Perfect for drop shippers</li>
+                                                    <li> Product research</li>
+                                                    <li> Product description writing</li>
+                                                    <li> Image editing</li>
+                                                    <li> Product listing and optimization</li>
+                                                    <li> Managing inventory</li>
+                                                    <li> Handling order processing</li>
+                                                    <li> Assistance in returns and exchanges</li>
+                                                    <li> Customer support</li>
+                                                </ul>
+                                            </ShowMore>
                                             <Anchor affix={false} style={{ display: "inline-block" }}>
                                                 <Link href="#booking" className="book-now" title="Request Quote" />
                                             </Anchor>
@@ -70,12 +113,16 @@ export default class Wework extends Component {
                                         <div className="item  fade-in-bottom-4">
                                             <img alt="no multichannel" src="https://lynagails-caters.s3-ap-southeast-1.amazonaws.com/uploads/Gostyle/multichannel-management.png" />
                                             <p>MultiChannel Management</p>
-                                            <ul>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                                <li>MultiChannel Management</li>
-                                            </ul>
+                                            <ShowMore
+                                                lines={3}
+                                                more='Show more'
+                                                less='Show less'
+                                                anchorClass='showmore'>
+                                                <ul>
+                                                    <li>eCommerce Website Full Package</li>
+                                                    <li> All in one dashboard to manage your lazada, Shopee, website and Zilingo</li>
+                                                </ul>
+                                            </ShowMore>
                                             <Anchor affix={false} style={{ display: "inline-block" }}>
                                                 <Link href="#booking" className="book-now" title="Request Quote" />
                                             </Anchor>
